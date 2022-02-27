0f1e2eb055025caa487dd4dfd312f25ce4857d6b
fubark <fubar.web@gmail.com>
2022-02-27 12:35:57 -0800

    test: Separate test templates.

---
+++ test/scratch_test.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+const stdx = @import("stdx");
+const t = stdx.testing;
+const log = stdx.log.scoped(.scratch);
+
+// Playground to test zig code.
+// Run with: zig build test-file -Dpath="test/scratch_test.zig"
+
+test {
+    t.setLogLevel(.debug);
+}
