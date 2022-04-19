4665deddde12b25e15754fc54616185e6d3f8439
fubark <fubar.web@gmail.com>
2022-04-18 18:19:23 -0700

    graphics: Add test and todos for cbez.

--- graphics/src/curve.zig
+++ graphics/src/curve.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 const stdx = @import("stdx");
+const t = stdx.testing;
 const Vec2 = stdx.math.Vec2;
 const vec2 = Vec2.init;
 const math = std.math;
+const trace = stdx.debug.tracy.trace;
+const traceN = stdx.debug.tracy.traceN;
 
 const log = stdx.log.scoped(.curve);
 
@@ -43,10 +46,10 @@ pub const QuadBez = struct {
     }
 
     /// Given t, return the (x, y) along the curve.
-    pub fn eval(self: Self, t: f32) Vec2 {
-        const mt = 1 - t;
-        const x = self.x0 * mt * mt + 2 * self.cx * t * mt + self.x1 * t * t;
-        const y = self.y0 * mt * mt + 2 * self.cy * t * mt + self.y1 * t * t;
+    pub fn eval(self: Self, t_: f32) Vec2 {
+        const mt = 1 - t_;
+        const x = self.x0 * mt * mt + 2 * self.cx * t_ * mt + self.x1 * t_ * t_;
+        const y = self.y0 * mt * mt + 2 * self.cy * t_ * mt + self.y1 * t_ * t_;
         return Vec2.init(x, y);
     }
 
@@ -55,6 +58,9 @@ pub const QuadBez = struct {
     /// This has an advantage over other methods since it determines the t values beforehand
     /// so that each t value can be evaluated in parallel, although the implementation is not taking advantage of that right now.
     pub fn flatten(self: Self, tol: f32, buf: *std.ArrayList(Vec2)) void {
+        const t__ = trace(@src());
+        defer t__.end();
+
         const params = self.mapToBasic();
         const a0 = approx_myint(params.x0);
         const a1 = approx_myint(params.x1);
@@ -73,8 +79,8 @@ pub const QuadBez = struct {
         var i: u32 = 1;
         while (i < n) : (i += 1) {
             const r = approx_inv_myint(a0 + ((a1 - a0) * @intToFloat(f32, i)) / @intToFloat(f32, n));
-            const t = (r - r0) / (r1 - r0);
-            buf.items[start + i] = self.eval(t);
+            const t_ = (r - r0) / (r1 - r0);
+            buf.items[start + i] = self.eval(t_);
         }
         buf.items[start + i] = vec2(self.x1, self.y1);
     }
@@ -121,12 +127,17 @@ pub const CubicBez = struct {
 
     /// Given error tolerance, output the minimum points needed to flatten the curve.
     /// The cubic bezier is first converted into quad bezier curves and then uses the same quad bezier flatten algorithm.
+    /// TODO: add comptime "continued" param to add points after the start point. Like a "curveTo".
     pub fn flatten(self: Self, tol: f32, buf: *std.ArrayList(Vec2), qbez_buf: *std.ArrayList(SubQuadBez)) void {
+        const t__ = trace(@src());
+        defer t__.end();
+
         const tol1 = 0.1 * tol; // error for subdivision into quads
         const tol2 = tol - tol1; // error for subdivision of quads into lines
         const sqrt_tol2 = math.sqrt(tol2);
         const err2 = self.weightsum(1, -3, 3, -1).squareLength();
         const n_quads = math.ceil(math.pow(f32, err2 / (432 * tol1 * tol1), @as(f32, 1)/@as(f32, 6)));
+        // n_quads is usually 2 or more quads.
         const n_quads_i = @floatToInt(u32, n_quads);
 
         qbez_buf.ensureTotalCapacity(n_quads_i) catch unreachable;
@@ -171,6 +182,7 @@ pub const CubicBez = struct {
         buf.items.len = start + ni + 1;
 
         buf.items[start] = vec2(self.x0, self.y0);
+        // TODO: See if kurbo's implementation is faster (https://github.com/linebender/kurbo/blob/master/src/bezpath.rs)
         var val: f32 = 0; // sum of vals from [0..i]
         i = 0;
         var j: u32 = 1;
@@ -187,8 +199,8 @@ pub const CubicBez = struct {
             const r1 = approx_inv_myint(a1);
             const a = a0 + (a1 - a0) * (target - val) / qbez_buf.items[i].val;
             const r = approx_inv_myint(a);
-            const t = (r - r0) / (r1 - r0);
-            buf.items[start + j] = qbez_buf.items[i].bez.eval(t);
+            const t_ = (r - r0) / (r1 - r0);
+            buf.items[start + j] = qbez_buf.items[i].bez.eval(t_);
         }
         buf.items[start + j] = vec2(self.x1, self.y1);
     }
@@ -211,21 +223,21 @@ pub const CubicBez = struct {
         };
     }
 
-    fn deriv(self: Self, t: f32) Vec2 {
-        const mt = 1 - t;
+    fn deriv(self: Self, t_: f32) Vec2 {
+        const mt = 1 - t_;
         const c0 = -3 * mt * mt;
-        const c3 = 3 * t * t;
-        const c1 = -6 * t * mt - c0;
-        const c2 = 6 * t * mt - c3;
+        const c3 = 3 * t_ * t_;
+        const c1 = -6 * t_ * mt - c0;
+        const c2 = 6 * t_ * mt - c3;
         return self.weightsum(c0, c1, c2, c3);
     }
 
-    fn eval(self: Self, t: f32) Vec2 {
-        const mt = 1 - t;
+    fn eval(self: Self, t_: f32) Vec2 {
+        const mt = 1 - t_;
         const c0 = mt * mt * mt;
-        const c1 = 3 * mt * mt * t;
-        const c2 = 3 * mt * t * t;
-        const c3 = t * t * t;
+        const c1 = 3 * mt * mt * t_;
+        const c2 = 3 * mt * t_ * t_;
+        const c3 = t_ * t_ * t_;
         return self.weightsum(c0, c1, c2, c3);
     }
 
@@ -242,3 +254,51 @@ pub const CubicBez = struct {
         };
     }
 };
+
+test "Cubic bezier flatten." {
+    const cbez = CubicBez{
+        .x0 = 200,
+        .y0 = 450,
+        .cx0 = 400,
+        .cy0 = 450,
+        .cx1 = 500,
+        .cy1 = 100,
+        .x1 = 600,
+        .y1 = 50,
+    };
+    var buf = std.ArrayList(Vec2).init(t.alloc);
+    defer buf.deinit();
+
+    var qbez_buf = std.ArrayList(SubQuadBez).init(t.alloc);
+    defer qbez_buf.deinit();
+
+    cbez.flatten(0.5, &buf, &qbez_buf);
+
+    log.warn("{any}", .{buf.items});
+
+    const exp: []const Vec2 = &.{
+        vec2(200, 450),
+        vec2(2.24316696e+02, 4.48221649e+02),
+        vec2(2.47434082e+02, 4.43284667e+02),
+        vec2(2.70417541e+02, 4.34863952e+02),
+        vec2(2.94486755e+02, 4.22462219e+02),
+        vec2(3.17744873e+02, 4.06724090e+02),
+        vec2(3.43482452e+02, 3.84952728e+02),
+        vec2(3.68587493e+02, 3.59402557e+02),
+        vec2(3.97798461e+02, 3.24399749e+02),
+        vec2(4.29767578e+02, 2.80172943e+02),
+        vec2(4.80043090e+02, 2.02067459e+02),
+        vec2(5.26117553e+02, 1.29646820e+02),
+        vec2(5.50330810e+02, 9.59812622e+01),
+        vec2(5.69920593e+02, 7.32915573e+01),
+        vec2(5.85504394e+02, 5.92830162e+01),
+        vec2(600, 50),
+    };
+
+    try t.eq(buf.items.len, exp.len);
+    var i: usize = 0;
+    while (i < buf.items.len) : (i += 1) {
+        try t.eqApprox(buf.items[i].x, exp[i].x, 1e-4);
+        try t.eqApprox(buf.items[i].y, exp[i].y, 1e-4);
+    }
+}
