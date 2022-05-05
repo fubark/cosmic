b908ba0e0c218999e041ffb165b0cdb87a29e35f
fubark <fubar.web@gmail.com>
2022-05-04 17:50:30 -0700

    ui: Add converter example app.

---
+++ ui/examples/converter.zig
@@ -0,0 +1,133 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const IsWasm = builtin.target.isWasm();
+const stdx = @import("stdx");
+const platform = @import("platform");
+const MouseUpEvent = platform.MouseUpEvent;
+const graphics = @import("graphics");
+const Color = graphics.Color;
+const ui = @import("ui");
+const importWidget = ui.Import.init;
+const Config = ui.Config;
+const WidgetRef = ui.WidgetRef;
+
+const Row = ui.widgets.Row;
+const Text = ui.widgets.Text;
+const Padding = ui.widgets.Padding;
+const Center = ui.widgets.Center;
+const TextField = ui.widgets.TextField;
+
+const helper = @import("helper.zig");
+const log = stdx.log.scoped(.main);
+
+pub const MyConfig = b: {
+    var config = Config{
+        .Imports = ui.widgets.BaseWidgets,
+    };
+    config.Imports = config.Imports ++ &[_]ui.Import{
+        importWidget(App),
+    };
+    break :b config;
+};
+
+pub const App = struct {
+    const Self = @This();
+
+    tc_field: WidgetRef(TextField),
+    tf_field: WidgetRef(TextField),
+
+    pub fn init(self: *Self, comptime C: Config, c: *C.Init()) void {
+        _ = self;
+        _ = c;
+    }
+
+    pub fn build(self: *Self, comptime C: Config, c: *C.Build()) ui.FrameId {
+        const S = struct {
+            fn onChangeTc(self_: *Self, text: []const u8) void {
+                const tc = std.fmt.parseFloat(f32, text) catch return;
+                self_.tf_field.widget.setValueFmt("{d:.2}", .{ tc * 9/5 + 32 });
+            }
+            fn onChangeTf(self_: *Self, text: []const u8) void {
+                const tf = std.fmt.parseFloat(f32, text) catch return;
+                self_.tc_field.widget.setValueFmt("{d:.2}", .{ (tf - 32) * 5 / 9 });
+            }
+        };
+
+        return c.decl(Center, .{
+            .child = c.decl(Row, .{
+                .expand = false,
+                .children = c.list(.{
+                    c.decl(TextField, .{
+                        .bind = &self.tc_field,
+                        .width = 200,
+                        .onChangeEnd = c.funcExt(self, []const u8, S.onChangeTc),
+                    }),
+                    c.decl(Padding, .{
+                        .child = c.decl(Text, .{
+                            .text = "Celsius =",
+                            .color = Color.White,
+                        }),
+                    }),
+                    c.decl(TextField, .{
+                        .bind = &self.tf_field,
+                        .width = 200,
+                        .onChangeEnd = c.funcExt(self, []const u8, S.onChangeTf),
+                    }),
+                    c.decl(Padding, .{
+                        .child = c.decl(Text, .{
+                            .text = "Fahrenheit",
+                            .color = Color.White,
+                        }),
+                    }),
+                }),
+            }),
+        });
+    }
+};
+
+var app: helper.App = undefined;
+var ui_mod: ui.Module(MyConfig) = undefined;
+
+pub fn main() !void {
+    // This is the app loop for desktop. For web/wasm see wasm exports below.
+    init();
+    defer deinit();
+    app.runEventLoop(update);
+}
+
+fn init() void {
+    app.init("Converter");
+    ui_mod.init(app.alloc, app.g);
+    ui_mod.addInputHandlers(&app.dispatcher);
+}
+
+fn deinit() void {
+    ui_mod.deinit();
+    app.deinit();
+}
+
+fn update(delta_ms: f32) void {
+    const S = struct {
+        fn buildRoot(_: void, c: *MyConfig.Build()) ui.FrameId {
+            return c.decl(App, .{});
+        }
+    };
+    const ui_width = @intToFloat(f32, app.win.getWidth());
+    const ui_height = @intToFloat(f32, app.win.getHeight());
+    ui_mod.updateAndRender(delta_ms, {}, S.buildRoot, ui_width, ui_height);
+}
+
+pub usingnamespace if (IsWasm) struct {
+    export fn wasmInit() *const u8 {
+        return helper.wasmInit(init);
+    }
+
+    export fn wasmUpdate(cur_time_ms: f64, input_buffer_len: u32) *const u8 {
+        return helper.wasmUpdate(cur_time_ms, input_buffer_len, &app, update);
+    }
+
+    /// Not that useful since it's a long lived process in the browser.
+    export fn wasmDeinit() void {
+        helper.wasmDeinit(deinit);
+    }
+} else struct {};
