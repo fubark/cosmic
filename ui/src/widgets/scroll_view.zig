c243cc799a282f0f4b01ee1083536861663e9ee4
fubark <fubar.web@gmail.com>
2022-05-17 07:08:57 -0700

    ui: Verticall scrollbar should take up space.

--- ui/src/widgets/scroll_view.zig
+++ ui/src/widgets/scroll_view.zig
@@ -9,18 +9,14 @@ const FrameListPtr = ui.FrameListPtr;
 
 const log = stdx.log.scoped(.scroll_view);
 
-/// Currently, the scrollbars do not contribute to the child container space and act like overlays.
-/// Doing so would require more complex logic and would either need to recompute the layout or defer recompute to the next frame.
-/// A similar effect can be achieved with padding or making the scrollbars thinner. People don't like big scrollbars anyway.
+/// By default, scroll view will stretch to it's child. Must be constrained by parent sizer (eg. Sized, Flex) to trigger scrollbars. 
 pub const ScrollView = struct {
-    const Self = @This();
-    const bar_size = 15;
-
     props: struct {
         child: ui.FrameId = ui.NullFrameId,
         bg_color: Color = Color.White,
         border_color: Color = Color.DarkGray,
-        pass_stretch_width: bool = false,
+        enable_hscroll: bool = true,
+        show_border: bool = true,
     },
 
     /// Internal vars. They should not be modified after the layout phase.
@@ -30,25 +26,42 @@ pub const ScrollView = struct {
     scroll_width: f32,
     scroll_height: f32,
 
+    eff_scroll_width: f32,
+    eff_scroll_height: f32,
+    has_vbar: bool,
+    has_hbar: bool,
+
     node: *Node,
+    scroll_to_bottom_after_layout: bool,
+
+    const Self = @This();
+    const BarSize = 15;
 
     pub fn init(self: *Self, comptime C: ui.Config, c: *C.Init()) void {
-        _ = c;
         self.scroll_x = 0;
         self.scroll_y = 0;
         self.scroll_width = 0;
         self.scroll_height = 0;
         self.node = c.node;
+        self.scroll_to_bottom_after_layout = false;
+        self.eff_scroll_width = 0;
+        self.eff_scroll_height = 0;
+        self.has_vbar = false;
+        self.has_hbar = false;
         c.addMouseScrollHandler(self, onMouseScroll);
     }
 
-    fn onMouseScroll(self: *Self, e: ui.Event(platform.MouseScrollEvent)) void {
+    fn onMouseScroll(self: *Self, e: ui.MouseScrollEvent) void {
         self.scroll_y += e.val.delta_y;
         self.checkScroll();
     }
 
     pub fn postUpdate(self: *Self) void {
         self.checkScroll();
+    }
+
+    pub fn scrollToBottomAfterLayout(self: *Self) void {
+        self.scroll_to_bottom_after_layout = true;
     }
 
     fn checkScroll(self: *Self) void {
@@ -81,21 +94,27 @@ pub const ScrollView = struct {
             it.layout.x = -scroll_x;
             it.layout.y = -scroll_y;
         }
+        self.computeEffScrollDims(node.layout.width, node.layout.height);
     }
 
-    /// Take up the same amount of space as it's children stacked over each other.
-    /// Records scroll width and height.
+    /// Take up the same amount of space as it's child or constrained by the parent.
+    /// Updates scroll width and height.
     pub fn layout(self: *Self, comptime C: ui.Config, c: *C.Layout()) ui.LayoutSize {
         const node = c.getNode();
         const size_cstr = ui.LayoutSize.init(std.math.inf(f32), std.math.inf(f32));
+
         self.scroll_height = 0;
         self.scroll_width = 0;
 
         if (self.props.child != ui.NullFrameId) {
             const child = node.children.items[0];
             var child_size: ui.LayoutSize = undefined;
-            if (c.prefer_exact_width and self.props.pass_stretch_width) {
-                child_size = c.computeLayoutStretch(child, ui.LayoutSize.init(c.getSizeConstraint().width, std.math.inf(f32)), true, false);
+            if (c.prefer_exact_width and !self.props.enable_hscroll) {
+                if (self.has_vbar) {
+                    child_size = c.computeLayoutStretch(child, ui.LayoutSize.init(c.getSizeConstraint().width - BarSize, std.math.inf(f32)), true, false);
+                } else {
+                    child_size = c.computeLayoutStretch(child, ui.LayoutSize.init(c.getSizeConstraint().width, std.math.inf(f32)), true, false);
+                }
             } else {
                 child_size = c.computeLayout(child, size_cstr);
             }
@@ -120,6 +139,21 @@ pub const ScrollView = struct {
         } else {
             res.cropToHeight(cstr.height);
         }
+
+        if (self.scroll_to_bottom_after_layout) {
+            if (self.scroll_height > res.height) {
+                self.scroll_y = self.scroll_height - res.height;
+            }
+            self.scroll_to_bottom_after_layout = false;
+        }
+
+        var prev_has_vbar = self.has_vbar;
+        self.computeEffScrollDims(res.width, res.height);
+        if (!prev_has_vbar and self.has_vbar and c.prefer_exact_width) {
+            // Recompute layout when turning on the vbar when prefer_exact_width is on.
+            return self.layout(C, c);
+        }
+
         return res;
     }
 
@@ -135,6 +169,16 @@ pub const ScrollView = struct {
         ctx.g.clipRect(alo.x, alo.y, alo.width, alo.height);
     }
 
+    /// Computes the effective scroll width/height.
+    fn computeEffScrollDims(self: *Self, width: f32, height: f32) void {
+        // The view will show more than the scroll height if the scroll y is close to the bottom.
+        self.eff_scroll_height = std.math.max(self.scroll_height, self.scroll_y + height);
+        self.eff_scroll_width = std.math.max(self.scroll_width, self.scroll_x + width);
+
+        self.has_vbar = height < self.eff_scroll_height;
+        self.has_hbar = width < self.eff_scroll_width;
+    }
+
     /// Computes the layout of the scrollbars here since it depends on layout phase completed to obtain the final ScrollView size.
     pub fn postRender(self: *Self, ctx: *ui.RenderContext) void {
         _ = self;
@@ -145,57 +189,52 @@ pub const ScrollView = struct {
         const alo = ctx.getAbsLayout();
 
         const g = ctx.getGraphics();
-        g.setStrokeColor(self.props.border_color);
-        g.setLineWidth(2);
-        g.drawRect(alo.x, alo.y, alo.width, alo.height);
-
-        // The view will show more than the scroll height if the scroll y is close to the bottom.
-        const eff_scroll_height = std.math.max(self.scroll_height, self.scroll_y + alo.height);
-        const eff_scroll_width = std.math.max(self.scroll_width, self.scroll_x + alo.width);
-
-        const draw_vert_bar = alo.height < eff_scroll_height;
-        const draw_horz_bar = alo.width < eff_scroll_width;
-
-        // Draw bottom right corner.
-        if (draw_vert_bar and draw_horz_bar) {
-            g.setFillColor(Color.LightGray);
-            g.fillRect(alo.x + alo.width - bar_size, alo.y + alo.height - bar_size, bar_size, bar_size);
+        if (self.props.show_border) {
+            g.setStrokeColor(self.props.border_color);
+            g.setLineWidth(2);
+            g.drawRect(alo.x, alo.y, alo.width, alo.height);
         }
 
-        if (draw_vert_bar) {
+        // Draw bottom right corner.
+        if (self.has_vbar and self.has_hbar) {
+            g.setFillColor(Color.LightGray);
+            g.fillRect(alo.x + alo.width - BarSize, alo.y + alo.height - BarSize, BarSize, BarSize);
+        }
+
+        if (self.has_vbar) {
             var max_bar_height = alo.height;
-            if (draw_horz_bar) {
-                max_bar_height -= bar_size;
+            if (self.has_hbar) {
+                max_bar_height -= BarSize;
             }
 
             // Draw vertical scrollbar.
             g.setFillColor(Color.LightGray);
-            g.fillRect(alo.x + alo.width - bar_size, alo.y, bar_size, max_bar_height);
+            g.fillRect(alo.x + alo.width - BarSize, alo.y, BarSize, max_bar_height);
 
             // Draw thumb.
-            const view_to_scroll_height = max_bar_height / eff_scroll_height;
+            const view_to_scroll_height = max_bar_height / self.eff_scroll_height;
             const vert_thumb_length = view_to_scroll_height * alo.height;
             const vert_thumb_y = view_to_scroll_height * self.scroll_y;
             g.setFillColor(Color.Gray);
-            g.fillRect(alo.x + alo.width - bar_size, alo.y + vert_thumb_y, bar_size, vert_thumb_length);
+            g.fillRect(alo.x + alo.width - BarSize, alo.y + vert_thumb_y, BarSize, vert_thumb_length);
         }
 
-        if (draw_horz_bar) {
+        if (self.has_hbar) {
             var max_bar_width = alo.width;
-            if (draw_vert_bar) {
-                max_bar_width -= bar_size;
+            if (self.has_vbar) {
+                max_bar_width -= BarSize;
             }
 
             // Draw horizontal scrollbar.
             g.setFillColor(Color.LightGray);
-            g.fillRect(alo.x, alo.y + alo.height - bar_size, max_bar_width, bar_size);
+            g.fillRect(alo.x, alo.y + alo.height - BarSize, max_bar_width, BarSize);
 
             // Draw thumb.
-            const view_to_scroll_width = max_bar_width / eff_scroll_width;
+            const view_to_scroll_width = max_bar_width / self.eff_scroll_width;
             const hor_thumb_length = view_to_scroll_width * alo.width;
             const hor_thumb_x = view_to_scroll_width * self.scroll_x;
             g.setFillColor(Color.Gray);
-            g.fillRect(alo.x + hor_thumb_x, alo.y + alo.height - bar_size, hor_thumb_length, bar_size);
+            g.fillRect(alo.x + hor_thumb_x, alo.y + alo.height - BarSize, hor_thumb_length, BarSize);
         }
     }
 };
