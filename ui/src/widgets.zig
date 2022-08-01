9ad2c627504c4ed78c65acebf1bec450fb3ffb0a
fubar <fubar.web@gmail.com>
2022-08-01 10:39:38 -0700

    ui: Rename widget types.

--- ui/src/widgets.zig
+++ ui/src/widgets.zig
@@ -8,80 +8,89 @@ pub const ModalOverlay = @import("widgets/root.zig").ModalOverlay;
 
 /// General widgets.
 const slider = @import("widgets/slider.zig");
-pub const SliderUI = slider.Slider;
-pub const Slider = genBuildWithNoChild(SliderUI);
-pub const SliderFloatUI = slider.SliderFloat;
-pub const SliderFloat = genBuildWithNoChild(SliderFloatUI);
+pub const SliderT = slider.Slider;
+pub const Slider = genBuildWithNoChild(SliderT);
+pub const SliderFloatT = slider.SliderFloat;
+pub const SliderFloat = genBuildWithNoChild(SliderFloatT);
 const switch_ = @import("widgets/switch.zig");
-pub const SwitchUI = switch_.Switch;
-pub const Switch = genBuildWithNoChild(SwitchUI);
-pub const ColorPickerUI = @import("widgets/color_picker.zig").ColorPicker;
-pub const ColorPicker = genBuildWithNoChild(ColorPickerUI);
+pub const SwitchT = switch_.Switch;
+pub const Switch = genBuildWithNoChild(SwitchT);
+pub const ColorPickerT = @import("widgets/color_picker.zig").ColorPicker;
+pub const ColorPicker = genBuildWithNoChild(ColorPickerT);
 const progress = @import("widgets/progress.zig");
-pub const ProgressBarUI = progress.ProgressBar;
-pub const ProgressBar = genBuildWithNoChild(ProgressBarUI);
-pub const FileDialogUI = @import("widgets/file_dialog.zig").FileDialog;
-pub const FileDialog = genBuildWithNoChild(FileDialogUI);
+pub const ProgressBarT = progress.ProgressBar;
+pub const ProgressBar = genBuildWithNoChild(ProgressBarT);
+pub const FileDialogT = @import("widgets/file_dialog.zig").FileDialog;
+pub const FileDialog = genBuildWithNoChild(FileDialogT);
 
 /// Flex containers.
 const flex = @import("widgets/flex.zig");
-pub const ColumnUI = flex.Column;
-pub const Column = genBuildWithChildren(ColumnUI);
-pub const RowUI = flex.Row;
-pub const Row = genBuildWithChildren(RowUI);
-pub const FlexUI = flex.Flex;
-pub const Flex = genBuildWithChild(FlexUI);
+pub const ColumnT = flex.Column;
+pub const Column = genBuildWithChildren(ColumnT);
+pub const RowT = flex.Row;
+pub const Row = genBuildWithChildren(RowT);
+pub const FlexT = flex.Flex;
+pub const Flex = genBuildWithChild(FlexT);
 
 /// Various containers.
 const containers = @import("widgets/containers.zig");
-pub const SizedUI = containers.Sized;
-pub const Sized = genBuildWithChild(SizedUI);
-pub const PaddingUI = containers.Padding;
-pub const Padding = genBuildWithChild(PaddingUI);
-pub const CenterUI = containers.Center;
-pub const Center = genBuildWithChild(CenterUI);
-pub const StretchUI = containers.Stretch;
-pub const Stretch = genBuildWithChild(StretchUI);
-pub const ZStack = containers.ZStack;
-pub const ScrollViewUI = @import("widgets/scroll_view.zig").ScrollView;
-pub const ScrollView = genBuildWithChild(ScrollViewUI);
+pub const SizedT = containers.Sized;
+pub const Sized = genBuildWithChild(SizedT);
+pub const PaddingT = containers.Padding;
+pub const Padding = genBuildWithChild(PaddingT);
+pub const CenterT = containers.Center;
+pub const Center = genBuildWithChild(CenterT);
+pub const StretchT = containers.Stretch;
+pub const Stretch = genBuildWithChild(StretchT);
+pub const ZStackT = containers.ZStack;
+pub const ZStack = genBuildWithChildren(ZStackT);
+pub const ScrollViewT = @import("widgets/scroll_view.zig").ScrollView;
+pub const ScrollView = genBuildWithChild(ScrollViewT);
 const mouse_area = @import("widgets/mouse_area.zig");
-pub const MouseAreaUI = mouse_area.MouseArea;
-pub const MouseArea = genBuildWithChild(MouseAreaUI);
-pub const MouseHoverAreaUI = mouse_area.MouseHoverArea;
-pub const MouseHoverArea = genBuildWithChild(MouseHoverAreaUI);
-pub const MouseDragAreaUI = mouse_area.MouseDragArea;
-pub const MouseDragArea = genBuildWithChild(MouseDragAreaUI);
+pub const MouseAreaT = mouse_area.MouseArea;
+pub const MouseArea = genBuildWithChild(MouseAreaT);
+pub const MouseHoverAreaT = mouse_area.MouseHoverArea;
+pub const MouseHoverArea = genBuildWithChild(MouseHoverAreaT);
+pub const MouseDragAreaT = mouse_area.MouseDragArea;
+pub const MouseDragArea = genBuildWithChild(MouseDragAreaT);
 const list = @import("widgets/list.zig");
 pub const List = list.List;
-pub const ScrollListUI = list.ScrollList;
-pub const ScrollList = genBuildWithChildren(ScrollListUI);
+pub const ScrollListT = list.ScrollList;
+pub const ScrollList = genBuildWithChildren(ScrollListT);
+pub const ContainerT = containers.Container;
+pub const Container = genBuildWithChild(ContainerT);
+pub const PositionedT = containers.Positioned;
+pub const Positioned = genBuildWithChild(PositionedT);
+
+/// Window.
+pub const WindowT = @import("widgets/window.zig").Window;
+pub const Window = genBuildWithChild(WindowT);
 
 /// Buttons.
 const button = @import("widgets/button.zig");
-pub const ButtonUI = button.Button;
-pub const Button = genBuildWithChild(ButtonUI);
-pub const TextButtonUI = button.TextButton;
-pub const TextButton = genBuildWithNoChild(TextButtonUI);
+pub const ButtonT = button.Button;
+pub const Button = genBuildWithChild(ButtonT);
+pub const TextButtonT = button.TextButton;
+pub const TextButton = genBuildWithNoChild(TextButtonT);
 
 /// Text related.
-pub const TextUI = @import("widgets/text.zig").Text;
-pub const Text = genBuildWithNoChild(TextUI);
+pub const TextT = @import("widgets/text.zig").Text;
+pub const Text = genBuildWithNoChild(TextT);
 const text_area = @import("widgets/text_area.zig");
-pub const TextAreaUI = text_area.TextArea;
-pub const TextArea = genBuildWithNoChild(TextAreaUI);
+pub const TextAreaT = text_area.TextArea;
+pub const TextArea = genBuildWithNoChild(TextAreaT);
 const text_field = @import("widgets/text_field.zig");
-pub const TextFieldUI = text_field.TextField;
-pub const TextField = genBuildWithNoChild(TextFieldUI);
+pub const TextFieldT = text_field.TextField;
+pub const TextField = genBuildWithNoChild(TextFieldT);
 
 /// Option widgets.
 const options = @import("widgets/options.zig");
-pub const SwitchOptionUI = options.SwitchOption;
-pub const SwitchOption = genBuildWithNoChild(SwitchOptionUI);
-pub const SliderOptionUI = options.SliderOption;
-pub const SliderOption = genBuildWithNoChild (SliderOptionUI);
-pub const SliderFloatOptionUI = options.SliderFloatOption;
-pub const SliderFloatOption = genBuildWithNoChild(SliderFloatOptionUI);
+pub const SwitchOptionT = options.SwitchOption;
+pub const SwitchOption = genBuildWithNoChild(SwitchOptionT);
+pub const SliderOptionT = options.SliderOption;
+pub const SliderOption = genBuildWithNoChild (SliderOptionT);
+pub const SliderFloatOptionT = options.SliderFloatOption;
+pub const SliderFloatOption = genBuildWithNoChild(SliderFloatOptionT);
 
 fn genBuildWithNoChild(comptime Widget: type) fn (props: anytype) ui.FrameId {
     const S = struct {
