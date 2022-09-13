60057e0874139298bf9e0051b7ac3c81ee98de76
fubar <fubar.web@gmail.com>
2022-09-13 09:22:11 -0700

    cscript: Separate qjs mock functions.

---
+++ test/mock_qjs.zig
@@ -0,0 +1,13 @@
+
+export fn JS_GetPropertyUint32() void {}
+export fn JS_FreeCString() void {}
+export fn JS_NewRuntime() void {}
+export fn JS_NewContext() void {}
+export fn JS_SetContextOpaque() void {}
+export fn JS_Eval() void {}
+export fn JS_FreeContext() void {}
+export fn JS_FreeRuntime() void {}
+export fn __JS_FreeValue() void {}
+export fn JS_ToCStringLen2() void {}
+export fn JS_GetException() void {}
+export fn JS_IsFunction() void {}
